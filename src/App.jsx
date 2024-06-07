4bcf4e70b2bdf5993985af43a49c1b8385e24e79
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-06-07 22:54:31 +0800

    video 3 - component & props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import React from 'react'
+
+const Button = (props) => {
+  const { children = '...', variant = 'black' } = props;
+  return (
+    <button className={`h-10 px-6 font-semibold rounded-md bg-${variant} text-white`} type="submit">
+      {children}
+    </button>
+  );
+}
+
 function App() {
   return (
     <div className='flex justify-center bg-blue-600 min-h-screen items-center'>
-      <div>Hello World</div>
+      <div className='flex gap-x-3'>
+        <Button variant="red-500">Login</Button>
+        <Button variant="slate-500">Logout</Button>
+        <Button>Register</Button>
+        <Button></Button>
+      </div>
     </div>
   );
 }
