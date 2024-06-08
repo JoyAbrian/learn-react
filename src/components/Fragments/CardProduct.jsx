7df8081396a5d14a7f63d0b642a1af115066d3bc
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-06-08 21:58:13 +0800

    video 9 - Rendering Lists

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -3,9 +3,9 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
     const { children } = props;
     return (
-            <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2">
-                {children}
-            </div>
+        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
+            {children}
+        </div>
     )
 }
 
@@ -13,7 +13,7 @@ const Header = (props) => {
     const { image } = props;
     return (
         <a href="#">
-            <img src={image} alt="Product" className="p-8 rounded-t-lg"/>
+            <img src={image} alt="Product" className="p-8 rounded-t-lg h-80"/>
         </a>
     );
 };
@@ -21,7 +21,7 @@ const Header = (props) => {
 const Body = (props) => {
     const { title, children } = props;
     return (
-        <div className="px-5 pb-5">
+        <div className="px-5 pb-5 h-full">
             <a href="">
                 <h5 className="text-xl font-semibold tracking-tight text-white">{title}</h5>
                 <p className="text-s text-white">{children}</p>
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,26 +1,43 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+    {
+        id: 1,
+        name: "Sepatu Baru",
+        price: "Rp. 1.000.000",
+        image: "/images/shoes-1.jpg",
+        description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quisquam, voluptate.",
+    },
+    {
+        id: 2,
+        name: "Sepatu Lama",
+        price: "Rp. 800.000",
+        image: "/images/shoes-2.jpg",
+        description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quisquam, voluptate. Quisquam, voluptate. Quisquam, voluptate.",
+    },
+    {
+        id: 3,
+        name: "Sepatu Rusak",
+        price: "Rp. 1.600.000",
+        image: "/images/shoes-3.jpg",
+        description: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+];
+
 const ProductsPage = () => {
     return (
         <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes-1.jpg"/>
+            {products.map((product) => (
+                <CardProduct key={product.id}>
+                    <CardProduct.Header image={product.image}/>
 
-                <CardProduct.Body title="Sepatu Baru">
-                    lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quisquam, voluptate.
-                </CardProduct.Body>
+                    <CardProduct.Body title={product.name}>
+                        {product.description}
+                    </CardProduct.Body>
 
-                <CardProduct.Footer price="Rp. 1.000.000"/>
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes-1.jpg"/>
-
-                <CardProduct.Body title="Sepatu Baru">
-                    lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quisquam, voluptate.
-                </CardProduct.Body>
-
-                <CardProduct.Footer price="Rp. 1.000.000"/>
-            </CardProduct>
+                    <CardProduct.Footer price={product.price}/>
+                </CardProduct>
+            ))}
         </div>
     );
 };
