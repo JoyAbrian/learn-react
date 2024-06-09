6ae1fc19b26c80237fefaaa0bda383f4ba33975d
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-06-09 22:04:17 +0800

    video 14 - useEffect

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import Button from "../components/Elements/Button";
 import Counter from "../components/Fragments/Counter";
@@ -30,12 +30,22 @@ const products = [
 const email = localStorage.getItem('email');
 
 const ProductsPage = () => {
-    const [cart, setCart] = useState([
-        {
-            id: 1,
-            qty: 1,
+    const [cart, setCart] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
+    useEffect(() => {
+        setCart(JSON.parse(localStorage.getItem('cart')) || []);
+    }, []);
+
+    useEffect(() => {
+        if (cart.length > 0) {
+            const sum = cart.reduce((acc, item) => {
+                const product = products.find((product) => product.id === item.id);
+                return acc + product.price * item.qty;
+            }, 0);
+            setTotalPrice(sum);
+            localStorage.setItem('cart', JSON.stringify(cart));
         }
-    ]);
+    }, [cart]);
 
     const handleLogout = () => {
         localStorage.clear();
@@ -93,12 +103,13 @@ const ProductsPage = () => {
                                     </tr>
                                 );
                             })}
+                            <tr>
+                                <td colSpan={3}><b>Total</b></td>
+                                <td><b>{(totalPrice).toLocaleString('id-ID', {style: 'currency', currency:'IDR'})}</b></td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
-            </div>
-            <div className="mt-5 flex justify-center mb-5">
-                <Counter></Counter>
             </div>
         </Fragment>
     );
